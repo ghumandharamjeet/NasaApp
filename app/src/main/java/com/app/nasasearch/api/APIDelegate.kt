fd846b9c2252a4117bfd263161b7a4ba00628be0
Dharamjeet Ghuman <dharamjeetat18@gmail.com>
2021-11-03 22:21:45 -0700

    removing unneccessory files

--- app/src/main/java/com/app/nasasearch/api/APIDelegate.kt
+++
@@ -1,9 +0,0 @@
-package com.app.nasasearch.api
-
-import com.app.newstoday.api.RetrofitInstance
-
-class APIDelegate {
-
-    suspend fun searchQuery(searchQuery: String, pageNumber: Int) =
-        RetrofitInstance.api.searchQuery(searchQuery, pageNumber)
-}
